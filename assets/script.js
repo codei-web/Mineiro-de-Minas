c4b206ea8d1e37170cab981bc993decec771607d
CodeiWeb <codeiw3b@gmail.com>
2023-02-20 18:14:04 -0300

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+function clickAutomotivoMobile() {
+  if (cardAutoMobile.style.display == "block") {
+    cardAutoMobile.style.display = "flex";
+  } else {
+    cardAutoMobile.style.display = "flex";
+    cardEletroMobile.style.display = "none";
+  }
+}
+
+function clickEletricoMobile() {
+  if (cardEletroMobile.style.display == "flex") {
+    cardEletroMobile.style.display = "flex";
+  } else {
+    cardEletroMobile.style.display = "flex";
+    cardAutoMobile.style.display = "none";
+  }
+}
+
+function clickAutomotivoDesktop() {
+  if (cardAutoDesktop.style.display == "block") {
+    cardAutoDesktop.style.display = "flex";
+  } else {
+    cardAutoDesktop.style.display = "flex";
+    cardEletroDesktop.style.display = "none";
+  }
+}
+
+function clickEletricoDesktop() {
+  if (cardEletroDesktop.style.display == "flex") {
+    cardEletroDesktop.style.display = "flex";
+  } else {
+    cardEletroDesktop.style.display = "flex";
+    cardAutoDesktop.style.display = "none";
+  }
+}
